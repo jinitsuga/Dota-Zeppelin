d68a5e6cd0c7908dab4d27efaad579fa26aa6528
agu <iba.agustin@gmail.com>
2024-01-22 18:17:58 -0300

    add obtaining noticeable players from lvie games

--- commands/utility/live.js
+++ commands/utility/live.js
@@ -7,6 +7,7 @@ module.exports = {
     .setDescription("Gives info about the top games being played right now."),
   async execute(interaction) {
     const liveGames = await getLiveGames();
+
     console.log(liveGames);
 
     await interaction.reply("Here go live games.");
--- utility/dotaApi.js
+++ utility/dotaApi.js
@@ -3,14 +3,30 @@ const axios = require("axios");
 const openDotaUrl = "https://api.opendota.com/api";
 
 const getLiveGames = async () => {
-  const req = await fetch(`${openDotaUrl}/live`)
+  const reqResult = await fetch(`${openDotaUrl}/live`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      return data;
     })
     .catch((error) => {
       console.log(error);
     });
+
+  const players = reqResult.map((game) => {
+    let players = [];
+
+    game.players.map((player) => {
+      if (player.name) {
+        players.push(player.name);
+      }
+    });
+    return players;
+  });
+
+  console.log(players);
+
+  const avgMmrs = reqResult.map((game) => game.average_mmr);
+  return avgMmrs.sort((a, b) => b - a);
 };
 
 module.exports = { getLiveGames };
